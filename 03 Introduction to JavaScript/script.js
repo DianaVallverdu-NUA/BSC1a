7a929ced11c4b40865770e7325615efe41e32dca
hlbatt <66482158+hlbatt@users.noreply.github.com>
2024-10-08 13:08:43 +0100

    Adds Intro to JS examples

---
+++ 03 Introduction to JavaScript/script.js
@@ -0,0 +1,11 @@
+//create our variable myName here!
+
+
+
+//Process to access the header of our website
+
+/*
+let heading = document.getElementById("greeting");
+heading.innerHTML = `Hello, ${myName}!`;
+
+*/
